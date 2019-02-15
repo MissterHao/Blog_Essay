5a1201e47616b4d8460b9a2c814a0da75798b876
Henry Li <henryliking@gmail.com>
2019-02-15 17:12:14 +0800

    Init

---
+++ PyQt5 Serial/Sin-Cos-Anim/Episode 1/main.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.button = QtWidgets.QPushButton(self)
+        self.button.setText("Hello")
+        
+app = None
+window = None
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
